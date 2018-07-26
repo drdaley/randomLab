1169f24ef81198f87d09efe4de8aecb18301ca73
drdaley <mail.david.daley@gmail.com>
2018-07-26 10:12:46 -0400

    script now changes the ID to the doi

--- feedTheBibliography.py
+++ feedTheBibliography.py
@@ -1,15 +1,25 @@
 import requests
+import re
 baseURL = 'http://dx.doi.org/'
 outputFile = 'testOutput.bib'
 headers={'Accept':'application/x-bibtex'}
-
+pattern='{[a-zA-Z0-9_]+,'
+regexID = re.compile(pattern)
+regexDOI = re.compile('doi = {[a-zA-Z0-9/.]+}')
 # main loop
 while True:
     inp = input('DOI: ')
     response = requests.get(baseURL+inp, headers=headers)
     if response.ok:
+        print(response.text)
+        baseInput= response.text
+        findID = regexID.search(baseInput)
+        findDOI = regexDOI.search(baseInput)
+
+        newBIB = baseInput[:findID.start()+1] + findDOI.group(0)[6:-1] + baseInput[findID.end()-1:]
+        print(newBIB)
         with open(outputFile,'a') as output:
-            output.write(response.text)
+            output.write(newBIB)
     else:print('There was an issue getting the bibtex.')
     again = input('more? ("e" to exit)')
     if again == 'e':break
